a0d75b6f6bf29171a53c619ab8c57a4d495a52c7
Ibrahim Sajib <ibrahimsajib.bd@gmail.com>
2024-09-26 20:07:08 +0600

    Project Created

---
+++ BIMS.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BIMS.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
