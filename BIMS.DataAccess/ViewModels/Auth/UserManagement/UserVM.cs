fe52bf7a58dbb8e8915d2933cc8dc20927a63b1d
Ibrahim Sajib <ibrahimsajib.bd@gmail.com>
2024-09-27 18:58:27 +0600

    ViewModels Created for User-Management

---
+++ BIMS.DataAccess/ViewModels/Auth/UserManagement/UserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIMS.DataAccess.ViewModels.Auth.UserManagement
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
