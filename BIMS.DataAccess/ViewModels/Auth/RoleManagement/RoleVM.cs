3b185f3b38e610d74f8bd01522bbc5733ced37e1
Ibrahim Sajib <ibrahimsajib.bd@gmail.com>
2024-09-27 11:57:59 +0600

    ViewModels Added for Role-Management

---
+++ BIMS.DataAccess/ViewModels/Auth/RoleManagement/RoleVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIMS.DataAccess.ViewModels.Auth.RoleManagement
+{
+    public class RoleVM
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name is required")]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ BIMS.DataAccess/ViewModels/Auth/RoleManagement/UsersInRoleVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIMS.DataAccess.ViewModels.Auth.RoleManagement
+{
+    public class UsersInRoleVM
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
